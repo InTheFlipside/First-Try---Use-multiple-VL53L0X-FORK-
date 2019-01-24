6a8e1d55ed2874fbce736c0ab1e73e719dd20660
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-01-24 15:30:01 +0100

    Create lissage.ino

---
+++ lissage.ino
@@ -0,0 +1,29 @@
+
+int valeur = 0;
+int lissage = 0;
+int action = 0;
+void setup() { Serial.begin(9600); }
+
+void loop() {
+valeur = Serial.parseInt();
+
+if(valeur>10 && valeur<50) {
+  Serial.println("Entre 10 et 50"); 
+       Serial.println(lissage);
+       if(lissage=3)
+       {action = 1;}
+       else {lissage++;}     
+       
+       
+       
+                           }
+       
+if(valeur > 50) {
+  lissage = 0;  }
+
+
+if(action != 0) { Serial.println("COOOOOOOOOOOOOOOOOOOOOOOOOOOODE"); }
+
+
+delay(100);
+}
